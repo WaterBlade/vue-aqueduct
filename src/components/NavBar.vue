439205a342806e40e451885064eefaf5a8a1af64
WaterBlade <zengjiangtao_yt@126.com>
2019-12-01 22:02:07 +0800

    init workable project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-menu>
+        <el-menu-item index="1">水力学设计</el-menu-item>
+        <el-menu-item index="2">槽身设计</el-menu-item>
+        <el-menu-item index="3">实心墩设计</el-menu-item>
+        <el-menu-item index="4">空心墩设计</el-menu-item>
+        <el-menu-item index="5">扩大基础设计</el-menu-item>
+        <el-menu-item index="6">桩基础设计</el-menu-item>
+    </el-menu>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'nav-bar',
+});
+</script>
---
+++ src/views/Frame.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-container>
+        <el-header>渡槽辅助设计</el-header>
+        <el-container>
+        <el-aside width="200px"><nav-bar/></el-aside>
+        <el-main><router-view/></el-main>
+        </el-container>
+    </el-container>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import NavBar from '@/components/NavBar.vue';
+export default Vue.extend({
+    name: 'frame',
+    components: {
+        NavBar,
+    },
+});
+</script>
